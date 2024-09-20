ff4fdb637ee35ffc8ede3f80eec2b4b755e572c0
†村神† <CA01971172@st.kawahara.ac.jp>
2024-09-20 12:45:33 +0900

    update to deploy

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ export default function App(){
                     count is {count}
                 </button>
                 <p>
-                    Edit<code>src/App.tsx</code> and save to test HMR
+                    Edit <code>src/App.tsx</code> and save to test HMR
                 </p>
             </div>
             <p className="read-the-docs">
