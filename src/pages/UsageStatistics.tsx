d73c6ace1e13a213ac8f1e791c8c42c97129052a
†村神† <CA01971172@st.kawahara.ac.jp>
2024-09-27 10:55:32 +0900

    try to count up

--- src/pages/UsageStatistics.tsx
+++ src/pages/UsageStatistics.tsx
@@ -6,8 +6,12 @@ import {
     Title,
     Tooltip,
     Legend
-} from "chart.js";
-import { Bar } from "react-chartjs-2";
+} from "chart.js"
+import { Bar } from "react-chartjs-2"
+import { BOOTH_ID_LIST, SCHOOL_DATA } from "./../utils/config"
+import { Item } from "../utils/types"
+import { countFireworkIds } from "../utils/usageStatistics"
+import { analysisData } from "../utils/analysisData"
 
 ChartJS.register(
     CategoryScale,
@@ -16,7 +20,7 @@ ChartJS.register(
     Title,
     Tooltip,
     Legend
-);
+)
 
 const options = {
     responsive: true,
@@ -29,13 +33,19 @@ const options = {
             text: "スタンプポイント毎のスタンプが押された量の統計"
         }
     }
-};
+}
 
 
 /* データの統計を取る */
+// データの集計を取る
+const countedData: Record<string, number> = countFireworkIds(analysisData)
+console.log("analysisData", analysisData)
+console.log("countedData", countedData)
 
-const labels = ["January", "February", "March", "April", "May", "June", "July"];
-const data1 = [12, 11, 14, 52, 14, 32, 36];
+// 学校名等のデータを定義する
+const labels = BOOTH_ID_LIST.map((boothId) => SCHOOL_DATA[boothId].schoolName)
+const backgroundColor = BOOTH_ID_LIST.map((boothId) => SCHOOL_DATA[boothId].color)
+const data1 = Object.values(countedData)
 
 const data = {
     labels, // x軸のラベルの配列
@@ -43,18 +53,10 @@ const data = {
     {
         label: "学校名", // データセットのラベル
         data: data1,      // データの配列(labelsと要素数同じ)
-        backgroundColor: [ // 各データポイントごとに異なる色を指定
-            "rgba(255, 99, 132, 0.5)",
-            "rgba(54, 162, 235, 0.5)",
-            "rgba(75, 192, 192, 0.5)",
-            "rgba(153, 102, 255, 0.5)",
-            "rgba(255, 159, 64, 0.5)",
-            "rgba(255, 205, 86, 0.5)",
-            "rgba(201, 203, 207, 0.5)"
-            ],
-        },
-    ],
-};
+        backgroundColor // 各データポイントごとに異なる色を指定
+        }
+    ]
+}
 
 export default function UsageStatistics(){
     return (
---
+++ src/utils/usageStatistics.ts
@@ -0,0 +1,29 @@
+/* スタンプポイント毎のスタンプが押された量の統計に必要な関数 */
+
+import { BOOTH_ID_LIST } from "./config";
+import { Item } from "./types";
+
+//文字列配列のそれぞれの文字列をキーとし、値を特定の値とするオブジェクトを作成する
+function createObjectFromArray<T>(arr: string[], value: T): { [key: string]: T }{
+    return arr.reduce((acc, cur) => {
+        acc[cur] = value;
+        return acc;
+    }, {} as { [key: string]: T });
+};
+
+// データを集計する関数
+export function countFireworkIds(items: Item[]){
+    const idCount: Record<string, number> = createObjectFromArray(BOOTH_ID_LIST, 0);
+
+    items.forEach(item => {
+        const fireworkIds = Object.keys(item.fireworksData);
+
+        fireworkIds.forEach(id => {
+            if(idCount[id]){
+                idCount[id]++;
+            }
+        });
+    });
+
+    return idCount;
+};
